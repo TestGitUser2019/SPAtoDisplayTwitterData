3eea529a5dd34ab2748982e2691e2478f1110122
pavithra <pavithra.s@indecomm.net>
2019-09-20 19:02:18 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule } from '@angular/common/http'
+import { DatePipe } from '@angular/common'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetTwitterDataComponent } from './get-twitter-data/get-twitter-data.component';
+import {TwitterService} from './TwitterService'
 
-@NgModule({
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    GetTwitterDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,   
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [TwitterService,DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
